2578efa005fbd914563fe647c5336b62d069fb1d
RealLionelMuhire <muhirelionel@gmail.com>
2022-07-09 08:54:25 +0200

    Retrying

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 
 /**
  * _islower -> checks if the character is lowercase
- * Return: return 1 and 0 depending on the condition
+ * Return: return 1 and 0 depending on condition
  */
-int _islower(int c);
+int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
