8be1158f830eae90da5f360e03a2be950c36fcfa
RealLionelMuhire <muhirelionel@gmail.com>
2022-07-14 16:51:08 +0300

    R

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /*
- * reset_to_98 -> a function to update the reference of a pointer
+ * reset_to_98 => a function to update the reference of a pointer
  * @n: pointer to n to be updated
  * return 0;
  */
