b383b211fa2afab70f6bdbefe69a96079802da85
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-18 23:25:03 +0200

    Alx Leo s commits, C: -variables, If -- else, for, while & do while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point and prints alphabets except
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if ((ch != 'q') && (ch != 'e'))
+		{
+			putchar(ch);
+		}
+		else
+			;
+	}
+	putchar('\n');
+
+	return (0);
+}
