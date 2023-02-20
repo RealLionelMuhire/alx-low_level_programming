23f4f27579b4cc8fc96387c0d7dad9a1c61a5111
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-20 14:15:48 +0200

    Alx Leo s commits, C: -variables, If -- else, for, while & do while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints all possible different combinations of three digits
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int m, n, p;
+
+	for (m = 0; m <= 7; m++)
+	{
+		for (n = m + 1; n <= 8; n++)
+		{
+			for (p = n + 1; p <= 9; p++)
+			{
+				putchar(m + '0');
+				putchar(n + '0');
+				putchar(p + '0');
+
+				if ((p < 9) || (n < 8) || (m < 7))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
