29a6aa36cd9ef42ce0bac943ee0558bb25e93e71
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-21 23:25:20 +0200

    Alx Leo s commits,Loops and Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 -> prints 10 times the alphabet, in lowercase
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
  * Return: always 0 (success)
  */
 void print_alphabet_x10(void)
@@ -10,8 +11,8 @@ void print_alphabet_x10(void)
 	{
 		for (char c = 'a'; c <= 'z'; c++)
 		{
-			putchar(c);
+			_putchar(c);
 		}
+		_putchar('\n');
 	}
-	putchar('\n');
 }
