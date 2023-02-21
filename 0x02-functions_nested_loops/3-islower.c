116e5ed3c2ef24b4cd84d03739da0af2a42a8600
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-22 00:23:52 +0200

    Alx Leo s commits,Loops and Nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ * @c: letter to check
+ * Return: 1 if c is lowercase, and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int check = 0;
+	char p = 'a';
+
+	for (; p <= 'z'; p++)
+	{
+		if (p == 'i')
+		{
+			check = 1;
+		}
+		else
+			check = 0;
+	}
+	return (check);
+}
