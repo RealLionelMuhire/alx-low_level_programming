3d89aa06028c7a9010be9232d52a1cb3fd04ff08
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-21 17:49:36 +0200

    Alx Leo s commits,Loops and Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - entry point
@@ -9,6 +8,16 @@
 
 int main(void)
 {
-	puts("_putchar\n");
+	_putchar('0')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
