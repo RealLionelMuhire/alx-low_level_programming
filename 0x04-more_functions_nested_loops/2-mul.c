a0d5df84a39b6332e743e7b9b02a82c5e39ea601
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-23 12:45:53 +0200

    Alx Leo s commits, More nested loops and functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a and b
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
