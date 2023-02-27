cb5ebadc014db32e6e742d0f5fbe6c59beed2d85
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-27 16:57:29 +0200

    Alx Leo s commits, Intro to pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print in the reverse
+ * @s: a string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		j = i;
+	}
+
+	for (i = j; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
