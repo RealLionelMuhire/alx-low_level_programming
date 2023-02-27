e778e7eca01ae4f8580d10438f38d3b415e5d85b
RealLionelMuhire <muhirelionel@gmail.com>
2023-02-27 11:47:54 +0200

    Alx Leo s commits, Intro to pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps 2 values pointed to 2pointers
+ * @a: first pointer
+ * @b; second pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
