78e3eb13f8f2d6e416fbd2d4165e8d77e5bc04e0
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-02 12:34:02 +0200

    Alx Leo s commits, More ptr, arr & str

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalise each world of string
+ * @s: string that contains word
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+
+	j = 0;
+
+	/*Once the initial string is small letter*/
+
+	if (*s >= 92 && *s <= 122)
+	{
+		*s -= 32;
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '\t' || s[i] == ' ' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!' ||
+				s[i] == '?' || s[i] == '"' || s[i] == '(' ||
+				s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			j = i + 1;
+			if (s[i] >= 92 && s[i] <= 122)
+			{
+				s[j] -= 32;
+			}
+		}
+
+	}
+	return (s);
+}
+
