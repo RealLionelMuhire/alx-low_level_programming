3ddb25be0dfb14275c1589bb6e869c3dd0bc379e
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-07 11:14:24 +0200

    Alx Leo s commits, Even More PTR, ARR STR

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - deference s to to
+ * @s: pointer take the value of to
+ * @to: poiter of refence
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
