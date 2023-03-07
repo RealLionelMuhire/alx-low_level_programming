8b5a4a8351e3d4fa32f645106a582d3dbbec2b76
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-07 10:48:40 +0200

    Alx Leo s commits, Even More PTR, ARR STR

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the diagonal suma
+ * @a: array of matrix
+ * @size: size of matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j;
+	int sum1 = 0, sum2 = 0;
+
+	for (; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+	}
+
+	for (i = 0, j = size - 1; i < size; i++, j--)
+	{
+		sum2 += *(a + i * size + j);
+	}
+
+	printf("%d,", sum1);
+	printf("%d\n", sum2);
+}
