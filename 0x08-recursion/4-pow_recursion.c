281a8b96d10894aa58b16fb9550f84fd914432d0
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-08 14:08:46 +0200

    Alx Leo s commits, Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - computes the power of x
+ * @x: the number to compute its power
+ * @y: the power of that number
+ * Return: the power of x to y
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{ 
+	{
 		return (-1);
 	}
 
@@ -12,5 +19,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
