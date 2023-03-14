8091dbf398b7a561d6b8306d6dbf531214550f10
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-14 12:30:54 +0200

    Alx Leo s commits, malloc&Feee

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strdup - copy a string and return a duplicate
+ * @str: string to be copied
+ * Return: duplicate otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	char *n_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	n_str = (char *)malloc(strlen(str) - 1);
+
+	if (n_str == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(n_str, str);
+
+	return (n_str);
+}
