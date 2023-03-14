f40d984935ad6de851d3f5a27bc96820ba6f9218
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-15 00:46:59 +0200

    Alx Leo s commits, malloc&Feee

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,4 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - free the memory allocated on the grid
+ * @grid: a pointer to the grid
+ * @height: the height of the grid memory to be freed
+ */
 
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
