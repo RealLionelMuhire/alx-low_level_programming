83a28e032e09c85ddb1283af4e0dd6b958b61b73
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-23 23:39:30 +0200

    Sum Them all Function. Files: a 0-main.c 0-sum_them_all.c

--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,11 @@
 This dir contains exercises about variadic functions 
 
 There are more c script
+
+
+- **.**
+This dir contains exercises about variadic functions 
+
+- **_putchar**
+*  _putchar - writes the character c to stdout
+
---
+++ 0x10-variadic_functions/append_RE.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Check if README.md already exists and has more than 4 lines
+if [ -f README.md ] && [ "$(wc -l < README.md)" -gt 4 ]; then
+  # Delete all lines in README.md except the first four
+  sed -i '5,$ d' README.md
+fi
+
+# Loop over all directories and subdirectories in the current directory
+find . -type d -print0 | while read -d '' -r dir; do
+    # Check if README.md file exists in the current directory
+    if [ -f "$dir/README.md" ]; then
+        # Get the first line of the README.md file
+        first_line=$(head -n 1 "$dir/README.md")
+
+        # Add the directory name to the parent README.md file
+        parent_dir=$(dirname "$dir")
+        echo "- **$(basename "$dir")**" >> "$parent_dir/README.md"
+        echo "$first_line" >> "$parent_dir/README.md"
+        echo "" >> "$parent_dir/README.md"
+    fi
+done
+
+# Loop over all .c files in the current directory
+for file in *.c; do
+    # Check if the file exists and is not empty
+    if [ -s "$file" ]; then
+        # Check if the file contains any betty-style function descriptions
+        if grep -q "/\*\*\s*$" "$file"; then
+            # Get the file name without the extension
+            filename=$(basename "$file" .c)
+
+            # Add the file name to the README.md file
+            echo "- **$filename**" >> README.md
+
+            # Get all betty-style function descriptions and append them to the README.md file
+            grep -A1 "/\*\*\s*$" "$file" | sed 's/^ \*//g' | grep -v "/\*\*" | sed 's/^/* /g' >> README.md
+
+            # Add a blank line to separate the entries in the README.md file
+            echo "" >> README.md
+        fi
+    fi
+done
+
