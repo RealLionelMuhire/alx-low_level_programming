78bd9555b615e2e708b2a962373ec9c73e8986ae
RealLionelMuhire <muhirelionel@gmail.com>
2023-03-31 00:05:27 +0200

    Adding a new node. Files: c 2-add_node.c 2-main.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to a linked list
+ * @head: head of new node
+ * @str: string of nodei
+ * Return: a new node added otherwise NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
