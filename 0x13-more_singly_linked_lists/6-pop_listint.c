8d47e4403a7f8ae36e79101e573c0b0991a10fc6
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-03 15:32:11 +0200

    Poping. Files: 6-pop_listint.c g 5-free_listint2.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,16 +1,22 @@
 #include "lists.h"
 
+/**
+ * pop_listint - deletes the head of linked list and return its data
+ * @head: head node of linked list
+ * Return: the data of deleted head node
+ */
 int pop_listint(listint_t **head)
 {
+	listint_t *tmp;
 
-	if (!*head)
+	if (!head)
 	{
 		return (0);
 	}
-	else
-	{
-		return (*head->n);
-		*head->next = NULL;
-	}
-	return (*head->n);
+
+	tmp = *head;
+	return (tmp->n);
+	tmp->next = NULL;
+
+	return (tmp->n);
 }
