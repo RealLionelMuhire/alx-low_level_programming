52f7e79caee0179b5cb9567af04c90b8b467fb31
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-04 12:20:47 +0200

    Adding a node to the index. Files: 9-insert_nodeint.c j i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,37 +1,50 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @n: data for inserted node
  * @head: head node of linked list
- * @idx: index position
- * @n: data of new inserted node
+ * @idx: index place
+ * Return: added node otherwise NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *n_node;
-	unsigned int p = 0;
-
-	if (!*head)
+	/*declare n_node, tmp and conter*/
+	listint_t *n_node, *tmp;
+	unsigned int count = 0;
+	/*initialize n_node*/
+	n_node = (listint_t *)malloc(sizeof(listint_t));
+	if (!n_node)
 	{
 		return (NULL);
 	}
-
-	n_node = (listint_t *)malloc(sizeof(listint_t));
 	n_node->n = n;
-	n_node->next = NULL;
-	
-	tmp = *head;
-	while(tmp->next)
+
+	/*if index is 0*/
+	if (idx == 0 && !head)
 	{
-		if (p == idx)
+		*head = n_node;
+		return (n_node);
+	}
+	if (idx == 0 && *head)
+	{
+		n_node->next = *head;
+		return (n_node);
+	}
+	/*assign head to tmp*/
+	tmp = *head;
+	/*iterate to find the index*/
+	while (tmp)
+	{
+		if (count == idx - 1)
 		{
+			n_node->next = tmp->next;
 			tmp->next = n_node;
-			n_node = tmp->next;
-			n_node->next = tmp;
 			return (n_node);
 		}
+		count++;
 		tmp = tmp->next;
-		p++;
 	}
-	return (NULL);
+	return (n_node);
 }
