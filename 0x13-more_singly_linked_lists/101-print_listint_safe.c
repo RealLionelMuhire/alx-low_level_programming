f8b689b1e1107ff14f6f1e08997c453efda00563
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-05 23:50:59 +0200

    Free the node. Files: 102-free_listint_safe.c n 102-main.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,6 +16,11 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)head, head->n);
 		count++;
 
+		/**
+		 * the head address should be superior to head->next, because
+		 * it is allocated prior to head->next, if not then it means that
+		 * head->next is allocated prior to head, then there is a loop
+		 */
 		if (head > head->next)
 		{
 			head = head->next;
