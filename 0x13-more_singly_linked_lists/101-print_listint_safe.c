c6d6266cef85b0749641b613c967abe86995cd43
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-05 22:54:32 +0200

    Fixing Print Safe. Files: 101-print_listint_safe.c m j

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,25 +10,25 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
+	if (!head)
+		exit(98);
+
 	while (head)
 	{
 		/*print the data and the address and increament*/
 		printf("[%p] %d\n", (void *)head, head->n);
 		count++;
 
-		/**
-		 * Check if there is a loop, if address of tmp, is lower than the
-		 * the address tmp->next, it means that the tmp->next was
-		 * allocated before tmp, thus a loop is found
-		 */
 		if (head > head->next)
+		{
 			head = head->next;
+		}
 		else
 		{
 			/*prints the content of the next node and exit*/
 			head = head->next;
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			exit(98);
+			break;
 		}
 	}
 	return (count);
