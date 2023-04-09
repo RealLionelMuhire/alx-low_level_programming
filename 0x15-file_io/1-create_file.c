8811207a01cf5ca9c2142d41b3c1a49374345363
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-09 19:36:42 +0200

    Creating a file Contains A Null terminated string. Files: hello 1-create_file.c b

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file contains a NULL terminated string
+ * @filename: the file to be created
+ * @text_content: text that contains the file
+ * Return: 1 at success, otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int written_bytes;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		written_bytes = write(fd, text_content, strlen(text_content));
+
+		if (written_bytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
+	else
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
+}
