7d2f0e71b38cdddebcbaf18f198509cf1f54c573
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-09 23:25:06 +0200

    Creating File. Files: hello b 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	else if (filename)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	}
+	else
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 	{
