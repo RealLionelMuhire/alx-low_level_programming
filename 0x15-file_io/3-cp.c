c5b62a1cf62e78e1b3103a105560788c8837b1fa
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-10 23:06:45 +0200

    Trying cpy. Files: 3-cp.c cp Inc

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,45 +7,50 @@
  * Return: always 0 at success otherwise -1
  */
 
-
 int main(int argc, char *argv[])
 {
 	int fd_src, fd_dest, wr_byt, rd_byt;
 	char *buf[1024];
 
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
 	fd_src = open(argv[1], O_RDONLY);
-	rd_byt = read(fd_src, buf, 1024);
-
-	if (fd_src == -1 || rd_byt == -1 || !argv[1])
+	if (fd_src == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	wr_byt = write(fd_dest, buf, 1024);
-	if (fd_dest == -1 || wr_byt == -1 || !argv[2])
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	if (close(fd_src) == -1)
+	rd_byt = read(fd_src, buf, 1024);
+	if (rd_byt == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	else if (close(fd_dest) == -1)
+	while (rd_byt > 0)
+	{
+		wr_byt = write(fd_src, buf, rd_byt);
+		if (wr_byt != rd_byt)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
 		exit(100);
 	}
+	close(fd_dest);
+	close(fd_src);
 	return (0);
-
 }
