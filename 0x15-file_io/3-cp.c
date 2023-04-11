3e7e5d54a2c43d7fcea65f258e812ad6327e4e45
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-11 10:32:19 +0200

    Alx Leo-s commits

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,7 @@
  * @argv: arguments
  * Return: always 0 at success otherwise -1
  */
+void printerrors(char *message, char *filename, int exit_value);
 
 int main(int argc, char *argv[])
 {
@@ -20,37 +21,47 @@ int main(int argc, char *argv[])
 	}
 	fd_src = open(argv[1], O_RDONLY);
 	if (fd_src == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		printerrors("Error: Can't read from file", argv[1], 98);
+
 	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	
 	if (fd_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		printerrors("Error: Can't write to", argv[2], 99);
+	
 	rd_byt = read(fd_src, buf, 1024);
 	if (rd_byt == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		printerrors("Error: Can't read from file", argv[1], 98);
 	while (rd_byt > 0)
 	{
-		wr_byt = write(fd_src, buf, rd_byt);
+		wr_byt = write(fd_dest, buf, rd_byt);
+
 		if (wr_byt != rd_byt)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			printerrors("Error: Can't write to", argv[2], 99);
 	}
 	if (close(fd_dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
 		exit(100);
 	}
+
+	if (close(fd_src) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src);
+		exit(100);
+	}
 	close(fd_dest);
 	close(fd_src);
 	return (0);
+}
+
+/**
+ * printerrors - prints errors message and exit
+ * @message: error message
+ * @filename: file the caused error
+ * @exit_value: is the exit value
+ */
+void printerrors(char *message, char *filename, int exit_value)
+{
+	dprintf(STDERR_FILENO,"%s %s\n", message, filename);
+	exit(exit_value);
 }
