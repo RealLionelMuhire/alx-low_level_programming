b1b28d377927d4859b2b1204a8b45d75ab1e148b
RealLionelMuhire <muhirelionel@gmail.com>
2023-04-11 11:48:56 +0200

    Copy Fixing. Files: 3-cp.c Incitatous cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * main - entry point: copies from file1 to file2
- * @argc: arguments number
- * @argv: arguments
- * Return: always 0 at success otherwise -1
+ * printerrors - prints error and exit
+ * @message: msg
+ * @filename: fliname
+ * @exit_value: exit value
  */
-void printerrors(char *message, char *filename, int exit_value);
+
+void printerrors(char *message, char *filename, int exit_value)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	exit(exit_value);
+}
+
+/**
+ * main - entry points
+ * @argc: arguments #
+ * @argv: arguments value
+ * Return: always 0 at success
+ */
 
 int main(int argc, char *argv[])
 {
 	int fd_src, fd_dest, wr_byt, rd_byt;
 	char *buf[1024];
 
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s cp file_from file_to\n", argv[0]);
@@ -23,21 +34,20 @@ int main(int argc, char *argv[])
 	if (fd_src == -1)
 		printerrors("Error: Can't read from file", argv[1], 98);
 
-	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
 	if (fd_dest == -1)
 		printerrors("Error: Can't write to", argv[2], 99);
-	
-	rd_byt = read(fd_src, buf, 1024);
-	if (rd_byt == -1)
-		printerrors("Error: Can't read from file", argv[1], 98);
-	while (rd_byt > 0)
+
+	while ((rd_byt = read(fd_src, buf, 1024)) > 0)
 	{
 		wr_byt = write(fd_dest, buf, rd_byt);
 
 		if (wr_byt != rd_byt)
 			printerrors("Error: Can't write to", argv[2], 99);
 	}
+	if (rd_byt == -1)
+		printerrors("Error: Can't read from file", argv[1], 98);
 	if (close(fd_dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
@@ -54,14 +64,3 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
-/**
- * printerrors - prints errors message and exit
- * @message: error message
- * @filename: file the caused error
- * @exit_value: is the exit value
- */
-void printerrors(char *message, char *filename, int exit_value)
-{
-	dprintf(STDERR_FILENO,"%s %s\n", message, filename);
-	exit(exit_value);
-}
