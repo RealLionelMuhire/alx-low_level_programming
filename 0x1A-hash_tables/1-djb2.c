735077c33db772d2f2b23ed4870674692daadf59
RealLionelMuhire <muhirelionel@gmail.com>
2023-07-11 14:05:32 +0200

    djb2.................. algorith

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function that uses djb2 algorithm
+ * str - passed string
+ * Return: hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	size_t hash_value = 0;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash_value = ((hash_value << 5) + hash_value) + c;
+	}
+	return (hash_value);
+}
